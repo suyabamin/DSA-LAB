dfdc56d772941833a7b1f5f749fa12139eb24b7d
suyab-amin <suyabamins@gmail.com>
2025-07-29 21:07:39 +0600

    selection sort

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=10;
+cout<<"hello world"<<endl;
+cout<<"age"<<a<<endl;
+//input
+int age;
+cout<<"enter age=";
+cin>>age;
+cout<<"your age is"<<age;
+return 0;
+}
